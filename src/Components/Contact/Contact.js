603bb9207b3853f2f0370caa29e46b0de07db4bf
Hemlatab <hemlata.b@hotmail.co.uk>
2019-11-09 20:58:30 +0400

    Add form in contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,37 +1,50 @@
 import React from "react";
 import "./Contact.css";
-import { Col, Row, Button, Form, FormGroup} from "react-bootstrap";
-
+import { Col, Row, Button, Form, FormGroup } from "react-bootstrap";
 
 const Contact = () => {
   return (
     <section className="wrapper">
-    <Form>
-      <Row form>
-    
-        <Col md={12}>
-          <FormGroup>
-            <label for="Password"> Name</label>
-            <input type="password" name="password" id="Password" placeholder="Enter name" />
-          </FormGroup>
-        </Col>
+      <Form>
+        <Row form>
+          <Col md={12}>
+            <FormGroup>
+              <label for="Password"> Name</label>
+              <input
+                type="password"
+                name="password"
+                id="Password"
+                placeholder="Enter name"
+              />
+            </FormGroup>
+          </Col>
 
-        <Col md={12}>
-          <FormGroup>
-            <label for="Email"> Email</label>
-            <input type="email" name="message" id="Email" placeholder="Email" />
-          </FormGroup>
-        </Col>
-        <Col md={12}>
-        <FormGroup>
-        <label for="message"> Message</label>
-        <input type="textarea" name="text" id="message" placeholder="Message"/>
-      </FormGroup>
-        </Col>
-      </Row>
-     
-      <Button>Sign in</Button>
-    </Form>
+          <Col md={12}>
+            <FormGroup>
+              <label for="Email"> Email</label>
+              <input
+                type="email"
+                name="message"
+                id="Email"
+                placeholder="Email"
+              />
+            </FormGroup>
+          </Col>
+          <Col md={12}>
+            <FormGroup>
+              <label for="message"> Message</label>
+              <input
+                type="textarea"
+                name="text"
+                id="message"
+                placeholder="Message"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+
+        <Button>Send</Button>
+      </Form>
     </section>
   );
 };
