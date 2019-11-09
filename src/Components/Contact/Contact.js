9fd75a6e3da6a339695796f0e7e94e67f86c8579
Hemlatab <hemlata.b@hotmail.co.uk>
2019-11-09 15:26:07 +0400

    Add content to contact component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,37 @@
 import React from "react";
 import "./Contact.css";
+import { Col, Row, Button, Form, FormGroup} from "react-bootstrap";
+
 
 const Contact = () => {
   return (
     <section className="wrapper">
-      <h1>Contact</h1>
+    <Form>
+      <Row form>
+    
+        <Col md={12}>
+          <FormGroup>
+            <label for="Password"> Name</label>
+            <input type="password" name="password" id="Password" placeholder="Enter name" />
+          </FormGroup>
+        </Col>
+
+        <Col md={12}>
+          <FormGroup>
+            <label for="Email"> Email</label>
+            <input type="email" name="message" id="Email" placeholder="Email" />
+          </FormGroup>
+        </Col>
+        <Col md={12}>
+        <FormGroup>
+        <label for="message"> Message</label>
+        <input type="textarea" name="text" id="message" placeholder="Message"/>
+      </FormGroup>
+        </Col>
+      </Row>
+     
+      <Button>Sign in</Button>
+    </Form>
     </section>
   );
 };
