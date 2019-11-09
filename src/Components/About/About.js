515308c7ca55b015bb4997764389c66af2278956
Hemlatab <hemlata.b@hotmail.co.uk>
2019-11-09 21:26:29 +0400

    Add content in About section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,11 +6,15 @@ const About = () => {
     <section className="wrapper">
       <h1>About</h1>
       <p>
-      A self-motivated, creative, and ambitious IT professional with proficiency in React, PHP, JavaScript,HTML and CSS,
-      <br />ability to communicate effectively in a team setting, and possessing
-      <br />strong skills and ability in writing clean and efficient code, seeks to apply for the position Front End Developer.
-      <br />
-      You can view my code on GitHub.
+        A self-motivated, creative, and ambitious IT professional with
+        proficiency in React, PHP, JavaScript,HTML and CSS,
+        <br />
+        ability to communicate effectively in a team setting, and possessing
+        <br />
+        strong skills and ability in writing clean and efficient code, seeks to
+        apply for the position Front End Developer.
+        <br />
+        <p>You can view my code on GitHub.</p>
       </p>
     </section>
   );
