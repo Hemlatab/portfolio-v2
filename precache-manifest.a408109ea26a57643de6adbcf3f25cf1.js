a310b7d61c4b150bb5db97d6a34a46eac8352841
Hemlatab <hemlata.b@hotmail.co.uk>
2019-11-20 08:03:42 +0400

    Updates

---
+++ precache-manifest.a408109ea26a57643de6adbcf3f25cf1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c20f3624db74f0d8da8ce67d88ca4f",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "8ecd194b732bf10a94b6",
+    "url": "/portfolio-v2/static/css/main.2579f6f3.chunk.css"
+  },
+  {
+    "revision": "4f6bdf27ca68c97f430c",
+    "url": "/portfolio-v2/static/js/2.2c25037e.chunk.js"
+  },
+  {
+    "revision": "8ecd194b732bf10a94b6",
+    "url": "/portfolio-v2/static/js/main.be4ae94d.chunk.js"
+  },
+  {
+    "revision": "efcfc39addee94f9596e",
+    "url": "/portfolio-v2/static/js/runtime-main.f58e8236.js"
+  },
+  {
+    "revision": "266389f0acfa1be508157af98bf08f48",
+    "url": "/portfolio-v2/static/media/Initab.266389f0.png"
+  },
+  {
+    "revision": "50900978e0985597b1071c02bb743099",
+    "url": "/portfolio-v2/static/media/Speech-to-text.50900978.png"
+  },
+  {
+    "revision": "6553c33a5f6f08a7341952e54bbf0a51",
+    "url": "/portfolio-v2/static/media/fitness.6553c33a.jpg"
+  },
+  {
+    "revision": "a3546cfceb26bd232bbe90b917459ec9",
+    "url": "/portfolio-v2/static/media/losangelesst.a3546cfc.jpg"
+  },
+  {
+    "revision": "3fe530c85b6c2d4ee8092d0b91bf27e0",
+    "url": "/portfolio-v2/static/media/notes.3fe530c8.png"
+  },
+  {
+    "revision": "e5e26347a7e0f1d29d0eaeed3e27b0a3",
+    "url": "/portfolio-v2/static/media/software_engineer.e5e26347.svg"
+  }
+]);
